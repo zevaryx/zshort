9d1d6a9134ce2d762c574568844c05d9944aa187
Zevaryx <zevaryx@gmail.com>
2022-06-07 12:19:13 -0600

    Add default user creating on startup if no users exist

--- zshort/__init__.py
+++ zshort/__init__.py
@@ -1,20 +1,26 @@
+import logging
 import os
+from datetime import datetime
+from hashlib import sha3_224
 
+import ulid
+from argon2 import PasswordHasher
 from dotenv import load_dotenv
 from fastapi import FastAPI, Path, Response
 from fastapi.responses import RedirectResponse
 
 from zshort.api.v1 import router
 from zshort.db import connect, q
-from zshort.db.models import Short
+from zshort.db.models import Short, User
 
 app = FastAPI(title="ZShorts")
 app.include_router(router)
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
 
-@app.get("/{slug}")
+@app.get("/{slug}", include_in_schema=False)
 async def redirect(slug: str = Path(title="The slug to get")) -> Response:
     short = await Short.find_one(q(slug=slug))
     if short:
@@ -32,3 +38,15 @@ async def startup():
     port = os.environ.get("MONGO_PORT", 27017)
     uri = os.environ.get("MONGO_URI")
     connect(host=host, username=user, password=password, port=port, uri=uri)
+    if len(await User.find().to_list(None)) == 0:
+        h = sha3_224()
+        h.update(datetime.now().isoformat().encode("UTF8"))
+        pwd = h.hexdigest()
+        ph = PasswordHasher()
+        hash = ph.hash(pwd)
+        user = User(username="admin", hash=hash, admin=True)
+        await user.commit()
+        logger.info("No users found, default user created. Credentials saved in default.txt")
+        with open("default.txt", "w+") as f:
+            data = f"Username: {user.username}\nPassword: {pwd}\nToken: {user.token}"
+            f.write(data)
